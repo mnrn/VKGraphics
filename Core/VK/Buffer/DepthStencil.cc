fe6942b2d98e41a62de145bd00a32ed47a0eef3a
ayase <paper.airplane01@gmail.com>
2021-01-12 23:37:29 +0900

    ソースファイルとヘッダファイルで分離できていない部分を分離しました。

--- Core/VK/Buffer/DepthStencil.cc
+++ Core/VK/Buffer/DepthStencil.cc
@@ -5,7 +5,31 @@
 #include "VK/Instance.h"
 #include "VK/Swapchain.h"
 
-void DepthStencil::Create(const Instance &instance,
-                          const Swapchain &swapchain) {
-  const VkFormat format = FindDepthFormat(instance.physicalDevice);
+static std::optional<VkFormat>
+FindSupportedFormat(const std::vector<VkFormat> &candicates,
+                    VkImageTiling tiling, VkFormatFeatureFlags features,
+                    VkPhysicalDevice physicalDevice) {
+  for (const auto &format : candicates) {
+    VkFormatProperties props{};
+    vkGetPhysicalDeviceFormatProperties(physicalDevice, format, &props);
+    if ((tiling == VK_IMAGE_TILING_LINEAR &&
+         (props.linearTilingFeatures & features) == features) ||
+        (tiling == VK_IMAGE_TILING_OPTIMAL &&
+         (props.optimalTilingFeatures & features) == features)) {
+      return format;
+    }
+  }
+  return std::nullopt;
+}
+
+std::optional<VkFormat> DepthStencil::FindDepthFormat(VkPhysicalDevice physicalDevice) {
+  return FindSupportedFormat(
+      {VK_FORMAT_D32_SFLOAT, VK_FORMAT_D32_SFLOAT_S8_UINT,
+       VK_FORMAT_D24_UNORM_S8_UINT},
+      VK_IMAGE_TILING_OPTIMAL, VK_FORMAT_FEATURE_DEPTH_STENCIL_ATTACHMENT_BIT,
+      physicalDevice);
+}
+
+void DepthStencil::Create(const Instance &,
+                          const Swapchain &) {
 }
--- Core/VK/Buffer/DepthStencil.h
+++ Core/VK/Buffer/DepthStencil.h
@@ -8,30 +8,7 @@ struct Instance;
 struct Swapchain;
 
 struct DepthStencil {
-  static std::optional<VkFormat>
-  FindSupportedFormat(const std::vector<VkFormat> &candicates,
-                      VkImageTiling tiling, VkFormatFeatureFlags features,
-                      VkPhysicalDevice physicalDevice) {
-    for (const auto &format : candicates) {
-      VkFormatProperties props{};
-      vkGetPhysicalDeviceFormatProperties(physicalDevice, format, &props);
-      if ((tiling == VK_IMAGE_TILING_LINEAR &&
-           (props.linearTilingFeatures & features) == features) ||
-          (tiling == VK_IMAGE_TILING_OPTIMAL &&
-           (props.optimalTilingFeatures & features) == features)) {
-        return format;
-      }
-    }
-    return std::nullopt;
-  }
-
-  static std::optional<VkFormat> FindDepthFormat(VkPhysicalDevice physicalDevice) {
-    return FindSupportedFormat(
-        {VK_FORMAT_D32_SFLOAT, VK_FORMAT_D32_SFLOAT_S8_UINT,
-         VK_FORMAT_D24_UNORM_S8_UINT},
-        VK_IMAGE_TILING_OPTIMAL, VK_FORMAT_FEATURE_DEPTH_STENCIL_ATTACHMENT_BIT,
-        physicalDevice);
-  }
+  static std::optional<VkFormat> FindDepthFormat(VkPhysicalDevice physicalDevice);
 
   void Create(const Instance &, const Swapchain &);
 
