564bc7ed401acd39360881f872872543189b1993
ayase <paper.airplane01@gmail.com>
2021-01-17 15:19:11 +0900

    シェーダーを切り替えられていなかったので修正しました。

--- Projects/HelloTriangle/HelloTriangle.cc
+++ Projects/HelloTriangle/HelloTriangle.cc
@@ -40,7 +40,7 @@ struct Vertex {
 static const std::vector<Vertex> vertices{{{0.0f, -0.5f}, {1.0f, 0.0f, 0.0f}},
                                           {{0.5f, 0.5f}, {0.0f, 1.0f, 0.0f}},
                                           {{-0.5f, 0.5f}, {0.0f, 0.0f, 1.0f}}};
-static const std::vector<uint16_t> indices{0, 1, 2};
+static const std::vector<uint16_t> indices{2, 1, 0};
 
 //*-----------------------------------------------------------------------------
 // Overrides functions
