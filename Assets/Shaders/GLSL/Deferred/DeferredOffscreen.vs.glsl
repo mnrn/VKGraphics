1483994b64573192fe003c66d619c7a1c7082009
ayase <paper.airplane01@gmail.com>
2021-02-07 20:50:40 +0900

    PushConstantsを使用するように変更しました。

--- Assets/Shaders/GLSL/Deferred/DeferredOffscreen.vs.glsl
+++ Assets/Shaders/GLSL/Deferred/DeferredOffscreen.vs.glsl
@@ -5,19 +5,21 @@ layout (location = 1) in vec3 VertexColor;
 layout (location = 2) in vec3 VertexNormal;
 
 layout (binding = 0) uniform UniformBufferObject {
-    mat4 Model;
-    mat4 View;
-    mat4 Proj;
+    mat4 ViewProj;
 } ubo;
 
 layout (location = 0) out vec3 WorldPos;
 layout (location = 1) out vec3 Color;
 layout (location = 2) out vec3 Normal;
 
-void main () {
-    gl_Position = ubo.Proj * ubo.View * ubo.Model * vec4(VertexPosition, 1.0);
+layout (push_constant) uniform PushConstants {
+    mat4 Model;
+} pushConsts;
 
-    WorldPos = vec3(ubo.Model * vec4(VertexPosition, 1.0));
+void main () {
+    gl_Position = ubo.ViewProj * pushConsts.Model * vec4(VertexPosition, 1.0);
+
+    WorldPos = vec3(pushConsts.Model * vec4(VertexPosition, 1.0));
     Color = VertexColor;
-    Normal = mat3(ubo.Model) * VertexNormal;
+    Normal = mat3(pushConsts.Model) * VertexNormal;
 }
